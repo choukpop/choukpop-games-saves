6daf4360bfaefb7dd2cd41f6fefa9a474490a7c9
Andre Costa <andrechougames1337kpopcosta@gmail.com>
2024-04-11 00:45:23 +0100

    update

---
+++ Kingdom Rush/kingdom_rush/global.lua
@@ -0,0 +1,7 @@
+local obj1 = {
+	["first_launch_time"] = 1712684743;
+	["marketing"] = {
+		["session_count"] = 6;
+	};
+}
+return obj1
---
+++ Kingdom Rush/kingdom_rush/settings.lua
@@ -0,0 +1,16 @@
+local obj1 = {
+	["fps"] = 60;
+	["fullscreen"] = false;
+	["height"] = 1080;
+	["highdpi"] = false;
+	["large_pointer"] = false;
+	["locale"] = "pt";
+	["msaa"] = 0;
+	["pause_on_switch"] = false;
+	["texture_size"] = "fullhd";
+	["volume_fx"] = 0.29734006734007;
+	["volume_music"] = 0.1306734006734;
+	["vsync"] = false;
+	["width"] = 1920;
+}
+return obj1
