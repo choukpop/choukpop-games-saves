57b042364fb57758b4572f167f2f07361e7b06b2
Andre Costa <andrechougames1337kpopcosta@gmail.com>
2024-12-02 19:20:45 +0000

    update

---
+++ Kingdom Rush/kingdom_rush/slot_2.lua
@@ -0,0 +1,236 @@
+local obj1 = {
+	["achievement_counters"] = {
+		["BLOODLUST"] = 56;
+		["CANNON_FODDER"] = 5;
+		["DEATH_FROM_ABOVE"] = 6;
+		["DIE_HARD"] = 147;
+		["EARN15_STARS"] = 3;
+		["EARN30_STARS"] = 3;
+		["EARN45_STARS"] = 3;
+		["EASY_TOWER_BUILDER"] = 12;
+		["FIRST_BLOOD"] = 56;
+		["GI_JOE"] = 9;
+		["HARD_TOWER_BUILDER"] = 12;
+		["MEDIUM_TOWER_BUILDER"] = 12;
+		["MULTIKILL"] = 56;
+		["SLAYER"] = 56;
+		["TACTICIAN"] = 1;
+		["WHATS_THAT"] = 2;
+	};
+	["achievements"] = {
+		["FIRST_BLOOD"] = true;
+		["IMPATIENT"] = true;
+	};
+	["bag"] = {
+	};
+	["difficulty"] = 1;
+	["gems"] = 86;
+	["heroes"] = {
+		["selected"] = "hero_elora";
+		["status"] = {
+			["hero_10yr"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_alleria"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_bolin"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_denas"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_elora"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_gerald"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_hacksaw"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_ignus"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_ingvar"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_magnus"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_malik"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_oni"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_thor"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+		};
+	};
+	["levels"] = {
+		[1] = {
+			[1] = 3;
+			[2] = 1;
+			["stars"] = 3;
+		};
+		[2] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[3] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[4] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[5] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[6] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[7] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[8] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[9] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[10] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[11] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[12] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[13] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[14] = {
+			[1] = 1;
+			[3] = 1;
+			["stars"] = 3;
+		};
+		[15] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[16] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[17] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[18] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[19] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[20] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[21] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[22] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[23] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[24] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[25] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+		[26] = {
+			[1] = 1;
+			["stars"] = 3;
+		};
+	};
+	["seen"] = {
+		["POWER_FIREBALL"] = true;
+		["POWER_REINFORCEMENT"] = true;
+		["TIP_ARMOR"] = true;
+		["TIP_HEROES"] = true;
+		["TIP_RALLY"] = true;
+		["TUTORIAL_1"] = true;
+		["enemy_fat_orc"] = true;
+		["enemy_goblin"] = true;
+		["tower_archer_1"] = true;
+		["tower_barrack_1"] = true;
+		["tower_build_archer"] = true;
+		["tower_build_barrack"] = true;
+		["tower_build_engineer"] = true;
+		["tower_build_mage"] = true;
+		["tower_engineer_1"] = true;
+		["tower_holder_grass"] = true;
+		["tower_holder_wasteland"] = true;
+		["tower_mage_1"] = true;
+	};
+	["upgrades"] = {
+		["archers"] = 5;
+		["barracks"] = 5;
+		["engineers"] = 5;
+		["mages"] = 5;
+		["rain"] = 5;
+		["reinforcements"] = 5;
+	};
+	["version_string"] = "kr1-desktop-5.6.12";
+}
+return obj1
