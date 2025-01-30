531a1b9670b431aa3d628bd8d3896d02fef22863
Andre Costa <andrechougames1337kpopcosta@gmail.com>
2025-01-30 20:13:14 +0000

    update

---
+++ Kingdom Rush/kingdom_rush_frontiers/slot_1.lua
@@ -0,0 +1,194 @@
+local obj1 = {
+	["achievements"] = {
+	};
+	["bag"] = {
+	};
+	["difficulty"] = 1;
+	["gems"] = 0;
+	["heroes"] = {
+		["selected"] = "hero_alric";
+		["status"] = {
+			["hero_alien"] = {
+				["skills"] = {
+					["abduction"] = 0;
+					["energyglaive"] = 0;
+					["finalcountdown"] = 0;
+					["purificationprotocol"] = 0;
+					["vibroblades"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_alric"] = {
+				["skills"] = {
+					["flurry"] = 0;
+					["sandwarriors"] = 0;
+					["spikedarmor"] = 0;
+					["swordsmanship"] = 0;
+					["toughness"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_beastmaster"] = {
+				["skills"] = {
+					["boarmaster"] = 0;
+					["deeplashes"] = 0;
+					["falconer"] = 0;
+					["regeneration"] = 0;
+					["stampede"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_crab"] = {
+				["skills"] = {
+					["battlehardened"] = 0;
+					["burrow"] = 0;
+					["hookedclaw"] = 0;
+					["pincerattack"] = 0;
+					["shouldercannon"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_dracolich"] = {
+				["skills"] = {
+					["bonegolem"] = 0;
+					["diseasenova"] = 0;
+					["plaguecarrier"] = 0;
+					["spinerain"] = 0;
+					["unstabledisease"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_dragon"] = {
+				["skills"] = {
+					["blazingbreath"] = 0;
+					["feast"] = 0;
+					["fierymist"] = 0;
+					["reignoffire"] = 0;
+					["wildfirebarrage"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_giant"] = {
+				["skills"] = {
+					["bastion"] = 0;
+					["boulderthrow"] = 0;
+					["hardrock"] = 0;
+					["massivedamage"] = 0;
+					["stomp"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_minotaur"] = {
+				["skills"] = {
+					["bloodaxe"] = 0;
+					["bullrush"] = 0;
+					["daedalusmaze"] = 0;
+					["doomspin"] = 0;
+					["roaroffury"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_mirage"] = {
+				["skills"] = {
+					["lethalstrike"] = 0;
+					["precision"] = 0;
+					["shadowdance"] = 0;
+					["shadowdodge"] = 0;
+					["swiftness"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_monk"] = {
+				["skills"] = {
+					["cranestyle"] = 0;
+					["dragonstyle"] = 0;
+					["leopardstyle"] = 0;
+					["snakestyle"] = 0;
+					["tigerstyle"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_monkey_god"] = {
+				["skills"] = {
+					["angrygod"] = 0;
+					["divinenature"] = 0;
+					["monkeypalm"] = 0;
+					["spinningpole"] = 0;
+					["tetsubostorm"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_pirate"] = {
+				["skills"] = {
+					["kraken"] = 0;
+					["looting"] = 0;
+					["scattershot"] = 0;
+					["swordsmanship"] = 0;
+					["toughness"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_priest"] = {
+				["skills"] = {
+					["blessedarmor"] = 0;
+					["consecrate"] = 0;
+					["divinehealth"] = 0;
+					["holylight"] = 0;
+					["wingsoflight"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_van_helsing"] = {
+				["skills"] = {
+					["beaconoflight"] = 0;
+					["holygrenade"] = 0;
+					["multishoot"] = 0;
+					["relicofpower"] = 0;
+					["silverbullet"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_voodoo_witch"] = {
+				["skills"] = {
+					["bonedance"] = 0;
+					["deathaura"] = 0;
+					["deathskull"] = 0;
+					["laughingskulls"] = 0;
+					["voodoomagic"] = 0;
+				};
+				["xp"] = 0;
+			};
+			["hero_wizard"] = {
+				["skills"] = {
+					["arcanefocus"] = 0;
+					["arcanereach"] = 0;
+					["chainspell"] = 0;
+					["disintegrate"] = 0;
+					["magicmissile"] = 0;
+				};
+				["xp"] = 0;
+			};
+		};
+	};
+	["levels"] = {
+		[1] = {
+		};
+	};
+	["seen"] = {
+		["heroroom_help"] = true;
+		["tower_acher_1"] = true;
+		["tower_barrack_1"] = true;
+		["tower_engineer_1"] = true;
+		["tower_mage_1"] = true;
+	};
+	["upgrades"] = {
+		["archers"] = 0;
+		["barracks"] = 0;
+		["engineers"] = 0;
+		["mages"] = 0;
+		["rain"] = 0;
+		["reinforcements"] = 0;
+	};
+	["version_string"] = "kr2-desktop-4.2.33";
+}
+return obj1
